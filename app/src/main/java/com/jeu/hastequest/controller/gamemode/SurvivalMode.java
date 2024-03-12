3a282b0dfc767de9a180cbe0e9f7aed1c361aca4
NoteletL <leo.notelet@gmail.com>
2024-03-12 23:25:21 +0100

    ajout du menu mode survie

--- app/src/main/java/com/jeu/hastequest/controller/gamemode/SurvivalMode.java
+++ app/src/main/java/com/jeu/hastequest/controller/gamemode/SurvivalMode.java
@@ -1,13 +1,26 @@
 package com.jeu.hastequest.controller.gamemode;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import androidx.cardview.widget.CardView;
 
+import com.jeu.hastequest.MainActivity;
 import com.jeu.hastequest.R;
 
 public class SurvivalMode extends GameMode{
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_menu);
+        setContentView(R.layout.survivalmenu);
+
+        ImageButton homeButton = findViewById(R.id.boutonHome);
+
+        android.view.View.OnClickListener listener = v -> {
+            if(v.getId() == R.id.boutonHome)
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        };
+
+        homeButton.setOnClickListener(listener);
     }
 }
